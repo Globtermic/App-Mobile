26cb0dd2edd2fdd5b1428ad6e5b43c598e95241b
nicolas humbert <nicolas.humbert@epitech.eu>
2023-04-07 17:15:28 +0200

    :alembic: change the button on the page for a better testing of the login page

--- Pages/Homepage.js
+++ Pages/Homepage.js
@@ -1,14 +1,18 @@
 import { Button, StyleSheet, Text, View, useColorScheme } from 'react-native';
 import { PageStyle } from '../Styles';
+import { useContext } from 'react';
+import { Context } from '../Token';
 
 export default function HomePage ({navigation}) {
+    const {token, setToken} = useContext(Context);
+
     return (
         <View style = {PageStyle.container}>
             <Text style = {PageStyle.text}>Welcome to Globtermic app!</Text>
             <Button
             color= 'black'
             title='Go Back'
-            onPress={() => navigation.navigate('Settings')}
+            onPress={() => setToken("")}
             /*onPress={() =>
                 navigation.navigate('Profile', {name: 'Jane'})}*/
             />
